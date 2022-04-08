18469addb33cddba0d418299b809f134d17a5317
José Miguel Candia <jmcandia@gmail.com>
2022-04-07 21:37:40 -0400

    Set up configuration module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  // Create NestJS application using AppModule module
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // Access configuration values from ConfigService
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT', 3000);
+  // Start the application
+  await app.listen(port);
 }
 bootstrap();
